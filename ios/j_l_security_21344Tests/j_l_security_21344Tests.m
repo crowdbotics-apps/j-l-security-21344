7878c145b0b8f8a273322a11237d6a5c5cb4b29f
Crowdbotics <team@crowdbotics.com>
2020-10-10 20:33:55 +0000

    Initial commit.

---
+++ ios/j_l_security_21344Tests/j_l_security_21344Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface j_l_security_21344 : XCTestCase
+
+@end
+
+@implementation j_l_security_21344
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
